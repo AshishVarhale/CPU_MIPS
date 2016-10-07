62ea2feb336e6c942483259711a418707958ce01
AshishVarhale <aavarhal@syr.edu>
2016-10-07 13:02:53 -0700

    Add files via upload

--- cpu.v
+++ cpu.v
@@ -1,28 +1,20 @@
-module cpu(clk, rst, out_data, ALU_result, rd_data1, rd_data2, mux_32_out, signExtend_out,Instructions, RegDst, RegWrite, ALUSrc, MemRead, MemWrite, MemtoReg, Branch, ALUOp, PC_out);
+module cpu(clk, rst, out_data);//,  ALU_result, mux_32_out, rd_data1, rd_data2, signExtend_out, PC_out, Instructions);
 	
 	input clk;
 	input rst;
 	
 	output [31:0] out_data;
 	
-	output [31:0] ALU_result;
-		output [31:0] rd_data1;///
-	output [31:0] rd_data2;///
-	output [31:0] mux_32_out;///
-	output [31:0] signExtend_out;///
-	output RegDst;
-	output RegWrite;
-	output ALUSrc;
-	output MemRead;
-	output MemWrite;
-	output MemtoReg;
-	output Branch;
-	output [1:0] ALUOp;
-	output [31:0] Instructions;
-	output [31:0] PC_out;
-	//output clk_90;
-
 	
+/*
+	output [31:0] ALU_result; // For debug
+	output [31:0] mux_32_out;
+	output [31:0] rd_data1;
+	output [31:0] rd_data2;
+	output [31:0] signExtend_out;
+	output [31:0] PC_out;
+	output [31:0] Instructions;
+	*/
 	//Signal Declarations:
 	
 	wire RegDst;
@@ -39,7 +31,7 @@ module cpu(clk, rst, out_data, ALU_result, rd_data1, rd_data2, mux_32_out, signE
 	
 	// Instantiations
 	
-	datapath DP(clk, rst, RegDst, RegWrite, ALUSrc, MemRead, MemWrite, MemtoReg, Branch, ALUOp, Opcode, out_data, ALU_result, rd_data1, rd_data2, mux_32_out, signExtend_out,Instructions, PC_out);
+	datapath DP(clk, rst, RegDst, RegWrite, ALUSrc, MemRead, MemWrite, MemtoReg, Branch, ALUOp, Opcode, out_data);//,  ALU_result, mux_32_out, rd_data1, rd_data2, signExtend_out, PC_out, Instructions);
 	
 	control_unit CU(Opcode, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
 	
